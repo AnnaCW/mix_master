64cf0577b9004b9ca9db31a8d79fbe9c5eba9b35
Anna Weisbrodt <aweisbro@gmail.com>
2016-05-30 17:25:09 -0600

    fix artist list alignment

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,6 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Artist.create(name: "Miles Davis", image_path: "https://pixabay.com/static/uploads/photo/2015/01/22/13/51/miles-davis-607733_960_720.jpg")
+Artist.create(name: "Miles Davis", image_path: "http://depositphotos.com/59817899/stock-photo-old-fashioned-turntable-playing-a.html")
 Artist.create(name: "Louis Armstrong", image_path: "https://pixabay.com/static/uploads/photo/2014/07/20/22/16/louis-armstrong-398146_960_720.jpg")
 Artist.create(name: "Ella Fitzgerald", image_path: "https://pixabay.com/en/ella-fitzgerald---female-portrait-1275909/")
